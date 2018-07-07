c7a074b3d085e3f172aeecf76e8ab9abc06ce299
ppontryagin <pavel.pontryagin@gmail.com>
2018-07-07 13:48:31 +0200

    web_server: add 5th error response

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # kubernetes-study
+
 kubernetes in action excersises
+
+How to run Erlang code
+
+```bash
+erlc server_tcp_static.erl && erl -s server_tcp_static start -s init stop
+```
--- ch04-kubia-unhealthy/erlang-server/server_tcp_static.erl
+++ ch04-kubia-unhealthy/erlang-server/server_tcp_static.erl
@@ -5,25 +5,34 @@ start() ->
     io:format("SERVER Trying to bind to port 2345~n"),
     {ok, Listen} = gen_tcp:listen(2345, [binary, {packet, 0}, {reuseaddr, true}, {active, true}]),
     io:format("SERVER Listening on port 2345~n"),
-    accept(Listen).
+    accept(Listen, 0).
 
-accept(Listen) ->
+accept(Listen, N) ->
     {ok, Socket} = gen_tcp:accept(Listen),
-    respond(Socket),
-    accept(Listen).
+    if
+        N == 4 ->
+            respond(Socket, bad_text_response()),
+            accept(Listen, 0);
+        true ->       
+            respond(Socket, good_text_response()),
+            accept(Listen, N+1)
+    end.
 
-respond(Socket) ->
+respond(Socket, Response) ->
     receive
-        {tcp, Socket, Bin} ->
-            Response = plain_text_response(),
-            gen_tcp:send(Socket, Response),
-            respond(Socket);
+        {tcp, Socket, _Bin} ->
+            gen_tcp:send(Socket, Response);
         {tcp_closed, Socket} ->
             io:format("SERVER: The client closed the connection~n")
     end.
 
-plain_text_response() ->
+good_text_response() ->
     {ok, Host} = inet:gethostname(),
     Msg = "You've hit " ++ Host ++ "\n",
     Length = integer_to_list(string:len(Msg)),
     "HTTP/1.1 200 OK\r\nContent-Length: " ++ Length ++ "\r\nContent-Type: text/plain\r\n\r\n" ++ Msg.
+
+bad_text_response() ->
+    Msg = "I'm not well. Please restart me!\n",
+    Length = integer_to_list(string:len(Msg)),
+    "HTTP/1.1 500 Internal Error\r\nContent-Length: " ++ Length ++ "\r\nContent-Type: text/plain\r\n\r\n" ++ Msg.
